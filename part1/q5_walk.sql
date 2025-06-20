5389e427c4377d055581d309527212382b45eb8c
fiskt <a1911280@adelaide.edu.au>
2025-06-20 02:02:13 +0000

    [automatic commit] 2025-06-20 02:02:13 UTC

--- part1/q5_walk.sql
+++ part1/q5_walk.sql
@@ -21,14 +21,14 @@ VALUES (
     (SELECT dog_id FROM Dogs WHERE name = "Min"),
     "2025-07-2 05:25:00",
     50,
-    "B",
+    "Parklands",
     "accepted"
 );
 
 INSERT INTO WalkRequests (dog_id, request_time, duration, location, status)
 VALUES (
-    (SELECT dog_id FROM Dogs WHERE name = "Bella"),
-    "2025-06-10 09:30:00",
+    (SELECT dog_id FROM Dogs WHERE name = "James"),
+    "2025-06-10 15:30:00",
     45,
     "Beachside Ave",
     "accepted"
