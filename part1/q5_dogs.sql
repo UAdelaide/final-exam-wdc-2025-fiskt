f29b236613b843ede74bb472407ee7b4ac12b3bf
fiskt <a1911280@adelaide.edu.au>
2025-06-20 01:52:42 +0000

    [automatic commit] 2025-06-20 01:52:42 UTC

--- part1/q5_dogs.sql
+++ part1/q5_dogs.sql
@@ -15,6 +15,13 @@ VALUES (
 INSERT INTO Dogs (owner_id, name, size)
 VALUES (
     SELECT user_id FROM Users WHERE username = "alice123",
+    "Min",
+    "large"
+);
+
+INSERT INTO Dogs (owner_id, name, size)
+VALUES (
+    SELECT user_id FROM Users WHERE username = "charlie",
     "Max",
     "medium"
 );
