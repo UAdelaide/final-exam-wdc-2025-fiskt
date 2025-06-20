a2f660be274928f58418daec04e8cd1b506a85f7
fiskt <a1911280@adelaide.edu.au>
2025-06-20 11:49:08 +0000

    [automatic commit] 2025-06-20 11:49:08 UTC 175.45.84.198

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -1,3 +1,6 @@
+DROP DATABASE IF EXISTS DogWalkService;
+CREATE DATABASE DogWalkService;
+USE DogWalkService;
 CREATE TABLE Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
