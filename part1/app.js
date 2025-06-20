5cae59468e06e8e8dc12d7bea91cf24225e4300b
fiskt <a1911280@adelaide.edu.au>
2025-06-20 02:17:11 +0000

    [automatic commit] 2025-06-20 02:17:11 UTC

--- part1/app.js
+++ part1/app.js
@@ -42,7 +42,9 @@ app.use(function(err, req, res, next) {
 
 app.get('/api/dogs', async (req, res, next) => {
   const [rows] = await db.query(
-    'SELECT Dogs.name, Dogs.size, Users.username FROM Dogs JOIN Users on Users.user_id = Dogs.owner_id'
+    'SELECT Dogs.name, Dogs.size, Users.username
+    FROM Dogs
+    JOIN Users on Users.user_id = Dogs.owner_id'
   );
 });
 
