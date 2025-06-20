73e6c3bd5ba6c5818ad3933c4224308070d78f3d
fiskt <a1911280@adelaide.edu.au>
2025-06-20 01:45:53 +0000

    [automatic commit] 2025-06-20 01:45:53 UTC

--- part1/q5_users.sql
+++ part1/q5_users.sql
@@ -1,6 +1,6 @@
 INSERT INTO Users (username, email, password_hash, role) VALUES ("alice123", "alice@example.com", "hasehd123", "owner");
 INSERT INTO Users (username, email, password_hash, role) VALUES ("bobwalker", "bob@example.com", "hashed456", "walker");
-INSERT INTO Users (username, email, password_hash, role) VALUES ("carol", "bob@example.com", "hashed456", "walker");
+INSERT INTO Users (username, email, password_hash, role) VALUES ("carol123", "carol@example.com", "hashed789", "owner");
 
 INSERT INTO Users (username, email, password_hash, role) VALUES ("bobwalker", "bob@example.com", "hashed456", "walker");
 INSERT INTO Users (username, email, password_hash, role) VALUES ("bobwalker", "bob@example.com", "hashed456", "walker");
