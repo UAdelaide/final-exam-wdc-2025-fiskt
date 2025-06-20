28bb863ac9a1ebc22c7e5681619b956d466c1d5d
fiskt <a1911280@adelaide.edu.au>
2025-06-20 03:21:30 +0000

    [automatic commit] 2025-06-20 03:21:30 UTC 175.45.84.198

--- part1/app.js
+++ part1/app.js
@@ -79,7 +79,8 @@ app.get('/api/walkrequests/open', async (req, res, next) => {
 app.get('/api/walkers/summary', async (req, res, next) => {
   try {
     const [rows] = await db.query(`
-      SELECT Users.username as "walker_username", COUNT(WalkRatings.rating) as "total_ratings", AVG(WalkRatings.rating) as "average_rating", COUNT()
+      SELECT Users.username as "walker_username", COUNT(WalkRatings.rating) as "total_ratings", AVG(WalkRatings.rating) as "average_rating", COUNT(WalkRatings.rating) as "completed_walks"
+      FROM W
       `);
   } catch(err) {
     res.status(500).json({ message: "request error" });
