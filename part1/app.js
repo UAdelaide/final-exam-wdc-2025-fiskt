0deb1ecb5c8cf4a65d8e14d17abced5f6d224cdb
fiskt <a1911280@adelaide.edu.au>
2025-06-20 09:02:42 +0000

    [automatic commit] 2025-06-20 09:02:42 UTC 175.45.84.198

--- part1/app.js
+++ part1/app.js
@@ -34,7 +34,10 @@ let db;
     await connection.query('SOURCE dogwalks.sql');
     await connection.end();
 
-    db = await mysql 
+    db = await mysql.createConnection({
+      host: 'localhost',
+      user
+    })
   }
 })
 
