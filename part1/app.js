3ec760a4bc87a6e03b8c65c281e8ae35c1258689
fiskt <a1911280@adelaide.edu.au>
2025-06-20 11:41:17 +0000

    [automatic commit] 2025-06-20 11:41:17 UTC 175.45.84.198

--- part1/app.js
+++ part1/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var mysql = require('mysql2/promise');
-var fs = require('fs/promise');
+var fs = require('fs/promises');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
