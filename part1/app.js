a4c2cbae7808faf463325fb72f10793768b99fa4
fiskt <a1911280@adelaide.edu.au>
2025-06-20 02:26:21 +0000

    [automatic commit] 2025-06-20 02:26:21 UTC

--- part1/app.js
+++ part1/app.js
@@ -63,7 +63,7 @@ app.get('/api/walkrequests/open', async (req, res, next) => {
       SELECT WR.request_id, Dog.name as "dog_name", WR.requested_time, WR.duration_minutes, WR.location, Users.username
       FROM WalkRequests as WR
       JOIN Dogs on Dogs.dog_id = WR.dog_id
-      JOIN Users on Users.user_Id
+      JOIN Users on Users.user_id 
       `);
   } catch(err) {
     res.status(500).json({ message: "request error" });
