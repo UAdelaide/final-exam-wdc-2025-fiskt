4c1f2d8b6fc057a4e3ddc537f5889d26d8945032
fiskt <a1911280@adelaide.edu.au>
2025-06-20 11:55:21 +0000

    [automatic commit] 2025-06-20 11:55:21 UTC 175.45.84.198

--- part1/database.js
+++ part1/database.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    passowrd: '',
+    passowrd: 'root',
     database: 'DogWalkService'
 });
 
