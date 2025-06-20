c24fc11014c7956de5cec4857917441dbc47f53e
fiskt <a1911280@adelaide.edu.au>
2025-06-20 10:13:23 +0000

    [automatic commit] 2025-06-20 10:13:23 UTC 175.45.84.198

--- part2/app.js
+++ part2/app.js
@@ -9,7 +9,9 @@ const app = express();
 app.post('/login-user', (req, res) => {
     const { user_id, username, role } = req.body;
     req.session.user = {
-        session_uid: user_id;
+        session_uid: user_id,
+        session_username: username,
+        session_role: role
     }
 });
 
