97edabe150926e975f5426c635f584656fcb73a3
fiskt <a1911280@adelaide.edu.au>
2025-06-20 02:33:51 +0000

    [automatic commit] 2025-06-20 02:33:51 UTC

--- part1/app.js
+++ part1/app.js
@@ -79,7 +79,7 @@ app.get('/api/walkrequests/open', async (req, res, next) => {
 app.get('/api/walkers/summary', async (req, res, next) => {
   try {
     const [rows] = await db.query(`
-      SELECT Users.username as walker_username, 
+      SELECT Users.username as walker_username, WalkRatings
       `);
   } catch(err) {
     res.status(500).json({ message: "request error" });
