79d4231381ab0c7d6c77e28fe03cdf17ceb48198
fiskt <a1911280@adelaide.edu.au>
2025-06-20 02:31:48 +0000

    [automatic commit] 2025-06-20 02:31:48 UTC

--- part1/app.js
+++ part1/app.js
@@ -78,10 +78,9 @@ app.get('/api/walkrequests/open', async (req, res, next) => {
 
 app.get('/api/walkers/summary', async (req, res, next) => {
   try {
-
+    const 
   } catch(err) {
-    res.status(500).json({ message: "request error"
-})
+    res.status(500).json({ message: "request error" });
   }
 });
 
