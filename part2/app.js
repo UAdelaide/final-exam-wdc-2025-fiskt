160262c777df1e265c2153a58085cfd90b125a3e
fiskt <a1911280@adelaide.edu.au>
2025-06-21 04:05:23 +0000

    [automatic commit] 2025-06-21 04:05:22 UTC ;; communications error to 208.67.222.222#53: timed out

--- part2/app.js
+++ part2/app.js
@@ -30,7 +30,12 @@ app.post('/login-user', (req, res) => {
 
 app.post('/logout-user', (req, res) => {
     req.session.destroy((err) => {
-        if (err )
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error logging out');
+        } else {
+            
+        }
     })
 })
 
