c5af51bf1e9bacd8e92dde685c027f8886bb17db
fiskt <a1911280@adelaide.edu.au>
2025-06-21 05:37:50 +0000

    [automatic commit] 2025-06-21 05:37:50 UTC

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -62,11 +62,7 @@ router.post('/:id/apply', async (req, res) => {
 router.get('/select-dogs', async (req, res) => {
   try {
     const [result] = await db.query(`SELECT * FROM Dogs WHERE owner_id = ?`, [req.session.user.user_id]);
-    if (result.length > 0) {
-      res.status(200).json(result);
-    } else {
-      res.status(500).json({ message: "No records found." });
-    }
+    res.status(200).json(result);
   } catch(err) {
     res.status(500).send("error");
   }
