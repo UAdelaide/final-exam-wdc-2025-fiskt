7f0f7378fe08d97630a45629a392790c670a6309
fiskt <a1911280@adelaide.edu.au>
2025-06-21 03:27:28 +0000

    [automatic commit] 2025-06-21 03:27:28 UTC

--- part1/insert_data.sql
+++ part1/insert_data.sql
@@ -8,3 +8,5 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ("alice123", "alice@example.com", "hasehd123", "owner"),
 ("bobwalker", "bob@example.com", "hashed456", "walker"),
 ("carol123", "carol@example.com", "hashed789", "owner"),
+("david", "david@example.com", "hashed100", "walker"),
+("")
