d69f6df2e19b9efb39d138f2f72883402a8743a3
fiskt <a1911280@adelaide.edu.au>
2025-06-21 03:32:35 +0000

    [automatic commit] 2025-06-21 03:32:35 UTC ;; communications error to 208.67.222.222#53: timed out

--- part1/insert_data.sql
+++ part1/insert_data.sql
@@ -2,7 +2,7 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ("alice123", "alice@example.com", "hasehd123", "owner"),
 ("bobwalker", "bob@example.com", "hashed456", "walker"),
 ("carol123", "carol@example.com", "hashed789", "owner"),
-("david", "david@example.com", "hashed100", "owner"),
+("david123", "david@example.com", "hashed100", "owner"),
 ("walker1234", "walking@example.com", "iluvwalking", "walker")
 
 INSERT INTO Dogs (owner_id, name, size) VALUES
@@ -10,4 +10,4 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = "carol123"), "Bella", "small"),
 ((SELECT user_id FROM Users WHERE username = "carol123"), "Lucky", "large"),
 ((SELECT user_id FROM Users WHERE username = "david"), "Mini", "large"),
-((SELECT user_id FROM Users WHERE username = "carol123"), "Bella", "small"),
+((SELECT user_id FROM Users WHERE username = "david123"), "Bella", "small"),
