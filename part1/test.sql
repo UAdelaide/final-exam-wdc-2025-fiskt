9966a010151eb9a0b91e7dff6e324ebd58189f40
fiskt <a1911280@adelaide.edu.au>
2025-06-21 03:18:02 +0000

    [automatic commit] 2025-06-21 03:18:02 UTC

--- part1/test.sql
+++ part1/test.sql
@@ -2,4 +2,4 @@ INSERT INTO WalkRatings (request_id, walker_id, rating)
 VALUES
   (1, 1, 5),  -- walker1 rating
   (2, 1, 4),  -- walker1 rating again
-  (3, 2, 3);  -- walker2 rating
+  (3, 3, 3);  -- walker2 rating
