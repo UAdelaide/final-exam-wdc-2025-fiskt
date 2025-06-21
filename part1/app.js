61716bd5a78426604fb17a98d296447354258186
fiskt <a1911280@adelaide.edu.au>
2025-06-21 00:31:21 +0000

    [automatic commit] 2025-06-21 00:31:21 UTC 175.45.84.198

--- part1/app.js
+++ part1/app.js
@@ -65,7 +65,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { title: 'Error' });
 });
 
 app.get('/api/dogs', async (req, res, next) => {
