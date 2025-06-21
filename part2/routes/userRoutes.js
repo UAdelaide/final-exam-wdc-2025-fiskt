1e249d4013d7cccf3ffcae0e9878d5f21ff48163
fiskt <a1911280@adelaide.edu.au>
2025-06-21 04:01:02 +0000

    [automatic commit] 2025-06-21 04:01:02 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -38,7 +38,6 @@ router.get('/me', (req, res) => {
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
-  console.log("username: ", username, " passowrd: ", password);
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
