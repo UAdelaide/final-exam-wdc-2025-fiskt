b237be209932763b0bf2037f12e12bf8dc4b117a
fiskt <a1911280@adelaide.edu.au>
2025-06-21 03:38:07 +0000

    [automatic commit] 2025-06-21 03:38:07 UTC

--- part1/insert_data.sql
+++ part1/insert_data.sql
@@ -3,14 +3,14 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ("bobwalker", "bob@example.com", "hashed456", "walker"),
 ("carol123", "carol@example.com", "hashed789", "owner"),
 ("david123", "david@example.com", "hashed100", "owner"),
-("walker1234", "walking@example.com", "iluvwalking", "walker")
+("walker1234", "walking@example.com", "iluvwalking", "walker");
 
 INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = "alice123"), "Max", "medium"),
 ((SELECT user_id FROM Users WHERE username = "carol123"), "Bella", "small"),
 ((SELECT user_id FROM Users WHERE username = "carol123"), "Lucky", "large"),
 ((SELECT user_id FROM Users WHERE username = "david"), "Mini", "large"),
-((SELECT user_id FROM Users WHERE username = "david123"), "Daisy", "medium"),
+((SELECT user_id FROM Users WHERE username = "david123"), "Daisy", "medium");
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
 ((SELECT dog_id FROM Dogs WHERE name = "Max"), "2025-06-10 08:00:00", 30, "Parklands", "open"),
