5fa64b63b3b4e92cc9c2220fc96e145821eeb53b
fiskt <a1911280@adelaide.edu.au>
2025-06-21 02:55:59 +0000

    [automatic commit] 2025-06-21 02:55:59 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -6,4 +6,59 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+app.get('/api/dogs', async (req, res, next) => {
+  try {
+    const [rows] = await db.query(`
+      SELECT Dogs.name AS "dog_name", Dogs.size, Users.username AS "owner_username"
+      FROM Dogs
+      JOIN Users on Users.user_id = Dogs.owner_id
+      `);
+    if (rows.length > 0) {
+      res.status(200).json(rows);
+    } else {
+      res.status(500).json({ message: "No records found" });
+    }
+  } catch(err) {
+    res.status(500).json({ message: "Request error" });
+  }
+});
+
+app.get('/api/walkrequests/open', async (req, res, next) => {
+  try {
+    const [rows] = await db.query(`
+      SELECT WR.request_id, Dogs.name AS "dog_name", WR.requested_time, WR.duration_minutes, WR.location, Users.username
+      FROM WalkRequests AS WR
+      JOIN Dogs on Dogs.dog_id = WR.dog_id
+      JOIN Users on Users.user_id = Dogs.owner_id
+      WHERE WR.status = "open"
+      `);
+    if (rows.length > 0) {
+      res.json(rows);
+    } else {
+      res.status(500).json({ message: "No records found" });
+    }
+  } catch(err) {
+    res.status(500).json({ message: "request error" });
+  }
+});
+
+app.get('/api/walkers/summary', async (req, res, next) => {
+  try {
+    const [rows] = await db.query(`
+      SELECT Users.username AS "walker_username", COUNT(WRs.rating) AS "total_ratings", AVG(WRs.rating) AS "average_rating", COUNT(WRs.rating) AS "completed_walks"
+      FROM WalkRatings AS WRs
+      JOIN Users on Users.user_id = WRs.walker_id
+      GROUP BY Users.user_id
+      `);
+    if (rows.length > 0) {
+      res.status(200).json(rows);
+    } else {
+      res.status(500).json({ message: "No records found" });
+    }
+  } catch(err) {
+    res.status(500).json({ message: "request error" });
+  }
+});
+
 module.exports = router;
