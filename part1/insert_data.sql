1e1a5a4939e4c90f6f12a6d28f618521ff83d0e9
fiskt <a1911280@adelaide.edu.au>
2025-06-21 03:35:39 +0000

    [automatic commit] 2025-06-21 03:35:39 UTC

--- part1/insert_data.sql
+++ part1/insert_data.sql
@@ -13,3 +13,8 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = "david123"), "Daisy", "medium"),
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
+((SELECT dog_id FROM Dogs WHERE name = "Max"), "2025-06-10 08:00:00", 30, "Parklands", "open"),
+((SELECT dog_id FROM Dogs WHERE name = "Bella"), "2025-06-10 09:30:00", 45, "Beachside Ave", "accepted"),
+((SELECT dog_id FROM Dogs WHERE name = "Mini"), "2025-07-02 05:25:00", 50, "Parklands", "open"),
+((SELECT dog_id FROM Dogs WHERE name = "Lucky"), "2025-06-03 15:00:00", 35, "Beachside Ave", "completed"),
+((SELECT dog_id FROM Dogs WHERE name = "Daisy"), "2025-06-16 03:55:00", 90, "Beachside Ave", "cancelled");
