3cdea8a8bac57cd8038e1cd504c5d3addd651fa6
fiskt <a1911280@adelaide.edu.au>
2025-06-21 02:58:26 +0000

    [automatic commit] 2025-06-21 02:58:26 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -24,7 +24,7 @@ router.get('/dogs', async (req, res, next) => {
   }
 });
 
-router.get('/api/walkrequests/open', async (req, res, next) => {
+router.get('/walkrequests/open', async (req, res, next) => {
   try {
     const [rows] = await db.query(`
       SELECT WR.request_id, Dogs.name AS "dog_name", WR.requested_time, WR.duration_minutes, WR.location, Users.username
@@ -43,7 +43,7 @@ router.get('/api/walkrequests/open', async (req, res, next) => {
   }
 });
 
-router.get('/api/walkers/summary', async (req, res, next) => {
+router.get('/walkers/summary', async (req, res, next) => {
   try {
     const [rows] = await db.query(`
       SELECT Users.username AS "walker_username", COUNT(WRs.rating) AS "total_ratings", AVG(WRs.rating) AS "average_rating", COUNT(WRs.rating) AS "completed_walks"
