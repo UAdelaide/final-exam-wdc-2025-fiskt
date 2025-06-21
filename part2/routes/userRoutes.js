23ec6ecf660cc9e5c7585522cc1d69e83a201cf4
fiskt <a1911280@adelaide.edu.au>
2025-06-21 04:31:05 +0000

    [automatic commit] 2025-06-21 04:31:05 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -48,7 +48,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    // create session
+    // create session and send the json
     req.session.user = rows[0];
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
