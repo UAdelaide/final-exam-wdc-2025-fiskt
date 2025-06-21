4b28dd9e6761640220bd304cd2e6d8395b84880a
fiskt <a1911280@adelaide.edu.au>
2025-06-21 03:33:37 +0000

    [automatic commit] 2025-06-21 03:33:37 UTC

--- part1/insert_data.sql
+++ part1/insert_data.sql
@@ -12,4 +12,4 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = "david"), "Mini", "large"),
 ((SELECT user_id FROM Users WHERE username = "david123"), "Daisy", "medium"),
 
-INSERT INTO
+INSERT INTO WalkRequests (dog)
