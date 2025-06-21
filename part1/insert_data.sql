f9624888891b5653ff89679236b1ea88c9c6153a
fiskt <a1911280@adelaide.edu.au>
2025-06-21 03:28:20 +0000

    [automatic commit] 2025-06-21 03:28:20 UTC

--- part1/insert_data.sql
+++ part1/insert_data.sql
@@ -1,12 +1,6 @@
-INSERT INTO Users (username, email, password_hash, role) VALUES ("alice123", "alice@example.com", "hasehd123", "owner");
-INSERT INTO Users (username, email, password_hash, role) VALUES ("bobwalker", "bob@example.com", "hashed456", "walker");
-INSERT INTO Users (username, email, password_hash, role) VALUES ("carol123", "carol@example.com", "hashed789", "owner");
-INSERT INTO Users (username, email, password_hash, role) VALUES ("david", "david@example.com", "hashed100", "walker");
-INSERT INTO Users (username, email, password_hash, role) VALUES ("charlie", "charlie@example.com", "hashed203", "owner");
-
 INSERT INTO Users (username, email, password_hash, role) VALUES
 ("alice123", "alice@example.com", "hasehd123", "owner"),
 ("bobwalker", "bob@example.com", "hashed456", "walker"),
 ("carol123", "carol@example.com", "hashed789", "owner"),
 ("david", "david@example.com", "hashed100", "walker"),
-("walker1234", "walking@example.com", "")
+("walker1234", "walking@example.com", "iluvwalking", "walker")
