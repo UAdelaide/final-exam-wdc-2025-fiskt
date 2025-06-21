f454b5035b0c7d612b9e640f89c6cc8e414092f5
fiskt <a1911280@adelaide.edu.au>
2025-06-21 03:11:12 +0000

    [automatic commit] 2025-06-21 03:11:12 UTC

--- part1/database.js
+++ part1/database.js
@@ -1,6 +1,11 @@
 const mysql = require('mysql2/promise');
+const { pool } = require('../part2/models/db');
 
 const poll = mysql.createPool({
     host: 'localhost',
-    user
-})
+    user: 'root',
+    passowrd: '',
+    database: 'DogWalkService'
+});
+
+module.exports = pool;
